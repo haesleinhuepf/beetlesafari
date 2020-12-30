5adf47bfd9f3ecb3f28f94e0b5e2cbe691663944
haesleinhuepf <robert_haase@gmx.de>
2020-12-30 20:35:08 +0100

    limit measurement generator

--- beetlesafari/utils/_feature_vectors.py
+++ beetlesafari/utils/_feature_vectors.py
@@ -4,7 +4,7 @@ def feature_vectors(input : dict, key_selection : list = None):
 
     output = []
     for key in key_selection:
-        output.append(input[key][0])
+        output.append(input[key])
 
     import numpy as np
     data = np.asarray(output).T
@@ -18,6 +18,7 @@ def neighborized_feature_vectors(
 
 ):
     import pyclesperanto_prototype as cle
+    import numpy as np
 
     if key_selection is None:
         key_selection = input.keys()
@@ -29,12 +30,14 @@ def neighborized_feature_vectors(
         print("key", key)
         element = input[key]
 
-        output.append(element[0])
+        output.append(element)
+
+        element = cle.push_zyx(np.asarray([element]))
 
         for neighborhood in neighborhoods:
-            print("n")
-            neighborized_element = cle.mode_of_touching_neighbors(element, neighborhood, neighborized_element)
-            output.append(cle.pull_zyx(neighborized_element)[0])
+            #print("n")
+            #neighborized_element = cle.mode_of_touching_neighbors(element, neighborhood, neighborized_element)
+            #output.append(cle.pull_zyx(neighborized_element)[0])
 
             neighborized_element = cle.median_of_touching_neighbors(element, neighborhood, neighborized_element)
             output.append(cle.pull_zyx(neighborized_element)[0])
@@ -45,13 +48,13 @@ def neighborized_feature_vectors(
             neighborized_element = cle.mean_of_touching_neighbors(element, neighborhood, neighborized_element)
             output.append(cle.pull_zyx(neighborized_element)[0])
 
-            neighborized_element = cle.minimum_of_touching_neighbors(element, neighborhood, neighborized_element)
-            output.append(cle.pull_zyx(neighborized_element)[0])
+            #neighborized_element = cle.minimum_of_touching_neighbors(element, neighborhood, neighborized_element)
+            #output.append(cle.pull_zyx(neighborized_element)[0])
 
-            neighborized_element = cle.maximum_of_touching_neighbors(element, neighborhood, neighborized_element)
-            output.append(cle.pull_zyx(neighborized_element)[0])
+            #neighborized_element = cle.maximum_of_touching_neighbors(element, neighborhood, neighborized_element)
+            #output.append(cle.pull_zyx(neighborized_element)[0])
 
     import numpy as np
     data = np.asarray(output).T
-    print(data.shape)
+
     return data
