bf5f5c6e9a9cf3dbb7c245a46fdf24f5169dd85c
haesleinhuepf <robert_haase@gmx.de>
2021-01-07 22:38:53 +0100

    updated / split examples / demos

--- demo/time_lapses.py
+++ demo/time_lapses.py
@@ -16,7 +16,7 @@ spot_detection_threshold = 25
 
 n_timepoints = 2
 
-num_class_aelection = [2, 4, 5, 6, 7, 10]
+num_class_selection = [2, 4, 5, 6, 7, 10]
 
 output_dir = "C:/structure/temp/lund/"
 
@@ -28,104 +28,14 @@ print(cle.select_device("RTX"))
 import time
 timestamp = time.time()
 
-def from_dataset_to_raw_statistics(
-        cc_dataset : bs.ClearControlDataset,
-        start_time_in_seconds : float,
-        end_time_in_seconds : float = None,
-        num_timepoints : int = 1,
-        sigma_noise_removal : float = 2,
-        sigma_background_removal : float = 7,
-        spot_detection_threshold : float = 10
-):
-    data = None
-    spots_to_keep = None
 
-    if end_time_in_seconds is None or num_timepoints == 1:
-        # make sure we execute the loop below once
-        end_time_in_seconds = start_time_in_seconds + 0.1
-        delta_time = 0.1
-    else:
-        # make a delta_time that allows us to select 10 time points
-        delta_time = (end_time_in_seconds - start_time_in_seconds) / (num_timepoints + 1)
-
-    resampled_image = None
-    cells = None
-
-    for t in np.arange(start_time_in_seconds, end_time_in_seconds, delta_time):
-        print("DATA", t)
-
-        from beetlesafari import stopwatch
-
-        stopwatch()
-        index = cc_dataset.get_index_after_time(t)
-        resampled_image = cc_dataset.get_resampled_image(index, resampled_image)
-
-        stopwatch("load")
-
-        print(resampled_image.shape)
-
-        cells, spots = bs.segmentation(resampled_image, cells, sigma_noise_removal=sigma_noise_removal, sigma_background_removal=sigma_background_removal, spot_detection_threshold=spot_detection_threshold)
-
-        stopwatch()
-
-        touch_matrix, neighbors_of_neighbors, neighbors_of_neighbors_of_neighbors = bs.neighbors(cells)
-
-        stopwatch("determine neighbors")
-
-        #spots_to_keep = cle.binary_and(cells, spots, spots_to_keep)
-        #cle.multiply_images(spots_to_keep, cells, spots)
-
-        pointlist = cle.labelled_spots_to_pointlist(spots)
-
-        stopwatch("centroids")
-
-        # ---------------------------------
-        # Measurements
-        meausrements = bs.collect_statistics(
-            resampled_image, cells,
-            #neighbor_statistics=True,
-            #intensity_statistics=False,
-            #shape_statistics=False,
-            #delta_statistics=False,
-            touch_matrix=touch_matrix,
-            neighbors_of_neighbors=neighbors_of_neighbors,
-            neighbors_of_neighbors_of_neighbors=neighbors_of_neighbors_of_neighbors,
-            centroids=pointlist
-        )
-
-        stopwatch("collect statistics")
-
-        single_timepoint_data = bs.neighborized_feature_vectors(meausrements, [touch_matrix, neighbors_of_neighbors,
-                                                                               neighbors_of_neighbors_of_neighbors])
-
-        stopwatch("make feature vectors")
-
-        if data is None:
-            data = single_timepoint_data
-        else:
-            data = np.concatenate((data, single_timepoint_data), 0)
-
-        print("data shape", data.shape)
-
-    return {
-        'data':data,
-        'index':index,
-        'resampled_image':resampled_image,
-        'cells':cells,
-        'spots':spots,
-        'touch_matrix':touch_matrix,
-        'neighbors_of_neighbors':neighbors_of_neighbors,
-        'neighbors_of_neighbors_of_neighbors':neighbors_of_neighbors_of_neighbors,
-        'centroids':pointlist
-    }
-
-bundle = from_dataset_to_raw_statistics(cc_dataset, start_time_in_seconds, end_time_in_seconds, num_timepoints=n_timepoints, spot_detection_threshold=spot_detection_threshold)
+bundle = bs.from_dataset_to_raw_statistics(cc_dataset, start_time_in_seconds, end_time_in_seconds, num_timepoints=n_timepoints, spot_detection_threshold=spot_detection_threshold)
 data = bundle['data']
 print(data.shape)
 resampled_image = None
 mesh = None
 
-for num_classes in num_class_aelection:
+for num_classes in num_class_selection:
 
     #model = bs.k_means_clustering(data, num_classes)
     model = bs.gaussian_mixture_model(data, num_classes)
@@ -133,7 +43,7 @@ for num_classes in num_class_aelection:
 
     for t in np.arange(start_time_in_seconds, end_time_in_seconds, delta_time_in_seconds):
 
-        bundle = from_dataset_to_raw_statistics(cc_dataset, t, spot_detection_threshold=spot_detection_threshold)
+        bundle = bs.from_dataset_to_raw_statistics(cc_dataset, t, spot_detection_threshold=spot_detection_threshold)
         cells = bundle['cells']
         single_timepoint_data = bundle['data']
         touch_matrix = bundle['touch_matrix']
