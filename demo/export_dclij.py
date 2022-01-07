a1e09a9f479442536912be4745a8d175f61befa8
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-01-07 20:33:32 +0100

    data export script

---
+++ demo/export_dclij.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pyclesperanto_prototype as cle
+import beetlesafari as bs
+
+
+cc_dataset = bs.ClearControlDataset('C:/structure/data/2019-12-17-16-54-37-81-Lund_Tribolium_nGFP_TMR/')
+output_dir = "C:/structure/temp/lund/"
+#sigma_noise_removal = 2
+#sigma_background_removal = 17
+#spot_detection_threshold = 25
+delta_time_in_seconds = bs.minutes_to_seconds(5)
+start_time_in_seconds = bs.hours_to_seconds(0)
+end_time_in_seconds = bs.hours_to_seconds(36)
+
+table = {'time_in_s':[],
+         'original_index':[],
+         'index':[]}
+
+for i, t in enumerate(np.arange(start_time_in_seconds, end_time_in_seconds, delta_time_in_seconds)):
+    print("Analysing time point just after " + str(t) + " s")
+
+    from beetlesafari import stopwatch
+
+    stopwatch()
+    index = cc_dataset.get_index_after_time(t)
+    print("Time (h), index", bs.seconds_to_hours(t), index)
+    image = cc_dataset.get_image(index)
+
+    from skimage.io import imsave
+    imsave(output_dir + "lund_i" + (str(i).zfill(6)) + "_oi_" + (str(index).zfill(6)) + ".tif", image)
+
+    table['time_in_s'].append(t)
+    table['original_index'].append(index)
+    table['index'].append(index)
+
+import pandas as pd
+pd.DataFrame(table).to_csv(output_dir + "metadata.csv")
+
